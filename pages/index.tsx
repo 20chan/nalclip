31d1ac684ee4c6435be4de1b0f12aae3ffffcd61
20chan <2@0chan.dev>
2021-03-19 18:05:32 +0000

    swr로 데이터 fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Layout from '../components/Layout'
+import { Clip } from '../interfaces';
+import useSWR from 'swr';
 
-const IndexPage = () => (
-  <Layout title="nalcli">
-    <h1>nalclip</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const { data } = useSWR<Clip[], any>('/api/clip');
+  const clipsDisplay = data
+    ? data.map(x => <li key={x.id}>{x.id} {x.content}</li>)
+    : <i>uhoh</i>;
+  return (
+    <Layout title="nalcli">
+      <h1>nalclip</h1>
+      <ul>
+        { clipsDisplay }
+      </ul>
+    </Layout>
+  );
+}
 
 export default IndexPage
