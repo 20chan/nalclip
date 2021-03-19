349aebf2bea2833548d1290fe929910311a0f370
20chan <2@0chan.dev>
2021-03-19 19:01:33 +0000

    refreshInterval 1s

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ import Layout from '../components/Layout'
 import { Clip } from '../interfaces';
 
 const IndexPage = () => {
-  const { data, revalidate } = useSWR<Clip[], any>('/api/clip');
+  const fetcher = async (url: string) => await (await fetch(url)).json();
+  const { data, revalidate } = useSWR<Clip[], any>('/api/clip', fetcher, {
+    refreshInterval: 1000,
+  });
   const clipsDisplay = data
     ? data.map(x => <li key={x.id}>{x.id} {x.content} <button onClick={() => copy(x.content)}>COPY</button></li>)
     : <i>uhoh</i>;
