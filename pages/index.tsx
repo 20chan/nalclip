ce9a30a4859c7c23b376371f07103ac3ac87be79
20chan <2@0chan.dev>
2021-03-20 08:58:36 +0000

    데이터 table로 표시

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import useSWR from 'swr';
 import copy from 'copy-to-clipboard';
-import Layout from '../components/Layout'
 import { Clip } from '../interfaces';
+import React from 'react';
+import Head from 'next/head';
 
 const IndexPage = () => {
   const fetcher = async (url: string) => await (await fetch(url)).json();
@@ -9,16 +10,44 @@ const IndexPage = () => {
     refreshInterval: 1000,
   });
   const clipsDisplay = data
-    ? data.map(x => <li key={x.id}>{x.id} {x.content} <button onClick={() => copy(x.content)}>COPY</button></li>)
+    ? (
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>createdAt</th>
+          <th>content</th>
+          <th>COPY</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map(x =>
+            <tr key={x.id}>
+              <td>{x.id}</td>
+              <td>{x.createdAt}</td>
+              <td>{x.content}</td>
+              <td><button onClick={() => copy(x.content)}>COPY</button></td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+      )
     : <i>uhoh</i>;
   return (
-    <Layout title="nalcli">
+    <div>
+      <Head>
+        <title>nalclip</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <h1>nalclip</h1>
       <button onClick={revalidate}>refresh</button>
       <ul>
         { clipsDisplay }
       </ul>
-    </Layout>
+    </div>
   );
 }
 
